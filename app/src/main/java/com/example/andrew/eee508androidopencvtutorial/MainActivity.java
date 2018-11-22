37d590ae23eb8765b5cbec8a5929a3fbae11a8a0
andremil <awmiller.andros@gmail.com>
2018-11-21 20:49:24 -0800

    next step

--- app/src/main/java/com/example/andrew/eee508androidopencvtutorial/MainActivity.java
+++ app/src/main/java/com/example/andrew/eee508androidopencvtutorial/MainActivity.java
@@ -2,12 +2,111 @@ package com.example.andrew.eee508androidopencvtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2,
+        View.OnTouchListener
+{
+
+
+    //<editor-fold desc="misc members">
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private Mat mRgba;
+
+    //</editor-fold>
+
+    //<editor-fold desc="loader interface">
+    private LoaderCallbackInterface mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(MainActivity.this);
+                }
+            }
+        }
+    };
+    //</editor-fold>
+
+    //<editor-fold desc="Life Cycle">
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.opencv_tutorial_activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(mOpenCvCameraView!=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,this,mLoaderCallback);
+        }else{
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(mOpenCvCameraView!=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="CameraViewListener2 Interface">
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+    //</editor-fold>
 }
